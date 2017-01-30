51b4f9253b95c5337e894d5b44de820e01060599
Vigneshwar <haivicky@gmail.com>
2017-01-30 23:22:37 +0530

    Board.java almost done

--- src/com/Assignments/week4/Board.java
+++ src/com/Assignments/week4/Board.java
@@ -1,46 +1,109 @@
-package com.Assignments.week4;
+//package com.Assignments.week4;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Created by Vigneshwar_V on 1/30/2017.
  */
 public class Board {
+    private int[][] board;
+
     public Board(int[][] blocks){
         /*Constructor for the Board class. Initialises a board from a n*n array of blocks*/
+        this.board = blocks;
     }
 
     public int dimension(){
     /*Returns the board dimension n*/
+        if(board!=null)
+            return board.length;
+        else
+            return 0;
     }
 
     public int hamming(){
         /*Returns the number of blocks that are out of place*/
+        int count = 0, k=0;
+        for(int i=0;i<board.length;i++)
+            for(int j=0;j<board.length;j++){
+                k++;
+                if(board[i][j]==0)
+                    continue;
+                if(board[i][j]!=k)
+                    count++;
+            }
+        return count;
     }
 
     public int manhattan(){
         /*Return the sum of manhattan distance between blocks and goal board*/
+        int count = 0, k=0;
+        for(int i=0;i<board.length;i++)
+            for(int j=0;j<board.length;j++){
+                k++;
+                //if(board[i][j]==0)
+                //    continue;
+                if(board[i][j]!=k)
+                    
+            }
+        return count;
     }
 
     public boolean isGoal(){
         /*Returns true if Goal Board is reached*/
+        return true;
     }
 
     public Board twin(){
         /* Returns a board obtained by exchanging any pair of blocks */
+        //return new Board();
+        return null;
     }
 
     public boolean equals(Object y){
         /* Does this board equals y */
+        return true;
     }
 
-    public Iterable<Board> neighbors(){
+    //public Iterable<Board> neighbors(){
         /* All Neigboring boards */
-    }
+    //}
 
     public String toString(){
         /* Returns the string representation of this board */
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++)
+                System.out.print(board[i][j]+" ");
+            System.out.println();
+        }
+        return null;
     }
 
-    public static void main(String[] args){
-        /* Unit tests */
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+        initial.toString();
+        System.out.println("Hamming value - "+ initial.hamming());
+
+        // solve the puzzle
+       // Solver solver = new Solver(initial);
+
+        // print solution to standard output
+       // if (!solver.isSolvable())
+            //StdOut.println("No solution possible");
+        //else {
+            //StdOut.println("Minimum number of moves = " + solver.moves());
+            //for (Board board : solver.solution())
+              //  StdOut.println(board);
     }
+
 }
