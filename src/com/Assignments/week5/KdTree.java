e1663810d7bd71a50ba2932a3ffb34f3835103b0
Vigneshwar_V <haivicky@gmail.com>
2017-02-13 18:58:00 +0530

    KDTree.java skeleton done.

---
+++ src/com/Assignments/week5/KdTree.java
@@ -0,0 +1,14 @@
+package com.Assignments.week5;
+
+/**
+ * Created by Vigneshwar_V on 2/13/2017.
+ */
+public class KdTree {
+    public KdTree(){
+        /* Constructor for the KD Tree Class. */
+    }
+
+    public static void main(String args[]){
+
+    }
+}
